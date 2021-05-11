6e9ed815afd6c7815692a115280895958e0d1168
✯ Sʜᴏɴ ✯ <82267769+Shon-TG-Hub@users.noreply.github.com>
2021-05-11 17:18:19 +0530

    Update help_text.py

--- Plugins/help_text.py
+++ Plugins/help_text.py
@@ -1,41 +1,116 @@
-import os
 
-if bool(os.environ.get("WEBHOOK", False)):
-    from sample_config import Config
-else:
-    from config import Config
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
+import pyrogram
+from pyrogram import filters
+from bot import autocaption
+from config import Config
 from translation import Translation
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+ 
 
-from pyrogram import Client, filters
+#all buttons 
+
+#start buttons 
+
+start_button=InlineKeyboardMarkup(
+        [
+             
+              [
+                  InlineKeyboardButton("Help", callback_data = Help_user"), 
+                  InlineKeyboardButton("About", callback_data = "about_test")
+              ], 
+              [
+                  InlineKeyboardButton("Nexon Projects ❤", Url = "https"//telegram.me/Nexonhex")
+              ],
+              [
+                  InlineKeyboardButton("Dev", url = "https://telegram.me/itz_me_shon"), 
+                  InlineKeyboardButton("Support", url = "https://telegram.me/Nexonhex_grp")
+              ] 
+        ]
+)
+
+# help buttons
+
+help_button=InlineKeyboardMarkup(
+        [
+              [
+                InlineKeyboardButton("Support", url = "https://telegram.me/nexonhex_grp")
+              ], 
+              
+        ]
+
+) 
+
+# about button
+
+about_button=InlineKeyboardMarkup(
+        [
+              [
+                  InlineKeyboardButton("Dev", url = "https://telegram.me/itz_me_shon"), 
+                  InlineKeyboardButton("support", url = "https://telegram.me/nexonhex_grp")
+              ]
+        ]
+) 
+
+@client.on_message(filters.command("start") & filters.private)
+async def start(bot, cmd):
+      await bot.send_message(
+          chat_id = cmd.chat.id,
+          text = Translation.START_TEXT.format(cmd.from_user.first_name, Config.ADMIN_USERNAME), 
+          reply_to_message_id = cmd.message_id,
+          parse_mode = "markdown",
+          disable_web_page_preview = True, 
+          reply_markup = start_button
+      )
 
 
-@Client.on_message(filters.private & filters.command(["help"]))
-async def help_user(bot, update):
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.HELP_USER,
-        parse_mode="html",
-        disable_web_page_preview=True,
-        reply_to_message_id=update.message_id
-    )
+@client.on_message(filters.command("help") & filters.private)
+async def help(bot, cmd):
+      await bot.send_message(
+          chat_id = cmd.chat.id,
+          text = Translation.HELP_TEXT, 
+          reply_to_message_id = cmd.message_id,
+          parse_mode = "html",
+          disable_web_page_preview = True,
+          reply_markup = help_button           
+      )
 
 
-@Client.on_message(filters.private & filters.command(["start"]))
-async def start(bot, update):
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.START_TEXT,
-        reply_to_message_id=update.message_id
-    )
+@client.on_message(filters.command("about") & filters.private)
+async def about(bot, cmd):
+      await bot.send_message(
+          chat_id = cmd.chat.id,
+          text = Translation.ABOUT_TEXT, 
+          reply_to_message_id = cmd.message_id,
+          parse_mode = "markdown",
+          disable_web_page_preview = True, 
+          reply_markup = about_button
+      )
 
 
-@Client.on_message(filters.private & filters.command(["upgrade"]))
-async def upgrade(bot, update):
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.UPGRADE_TEXT,
-        parse_mode="html",
-        reply_to_message_id=update.message_id,
-        disable_web_page_preview=True
-    )
+
+# call_backs 
+
+@client.on_callback_query()
+async def button(bot, cmd: CallbackQuery):
+    cb_data = cmd.data
+    if "about_data" in cb_data:
+        await cmd.message.edit(
+             text = Translation.ABOUT_TEXT,
+             parse_mode="markdown", 
+             disable_web_page_preview=True, 
+             reply_markup=InlineKeyboardMarkup(
+                 [
+                     [
+                      InlineKeyboardButton("Dev", url="https://telegram.me/itz_me_shon"),
+                      InlineKeyboardButton("support", url="https://telegram.me/nexonhex_grp")
+                     ]
+ 
+                 ] 
+             ) 
+        )
+    
